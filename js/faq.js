e9a9c1330a9a78e37f89b3dc97c2286316364600
Joe Z <joefb@Joes-MacBook-Pro-2.local>
2019-12-11 15:20:42 -0500

    CHANGED: Copies and Typos;Added more contents in our story page

--- js/faq.js
+++ js/faq.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
     var container = '';
     if(faqQuestions.length == faqAnswers.length){
-        for(var i = 0; i <= faqQuestions.length; i++){
+        for(var i = 0; i < faqQuestions.length; i++){
             var template = '<div class="accordion" id="accordion'+i+'"><div class="faq-card"><div class="faq-card-header" id="faq'+i+'"><button class="faq-heading" type="button" data-toggle="collapse" data-target="#collapse'+i+'" aria-expanded="true" aria-controls="collapse'+i+'">'+faqQuestions[i]+'</button></div><div id="collapse'+i+'" class="collapse" aria-labelledby="faq'+i+'" data-parent="#accordion'+i+'"><div class="card-body">'+faqAnswers[i]+'</div></div></div></div>';
             container += template
         }
     }
+
+    // fixed a undefined issue here
+
     $('.faq-main').html(container);
 
     $('.faq-card-header').click(function(){
@@ -15,10 +18,10 @@ $(document).ready(function(){
         console.log('#collapse'+i)
         console.log($('#collapse'+i).hasClass('show'))
         if(!$('#collapse'+i).hasClass('show')){
-            $(target).css("background-image", "url('assets/img/drop-up.svg')") 
+            $(target).css("background-image", "url('assets/img/drop-up.svg')")
         }
         else{
-            $(target).css("background-image", "url('assets/img/drop-down.svg')") 
+            $(target).css("background-image", "url('assets/img/drop-down.svg')")
         }
         // if($('#collapse'+id).hasClass('show'))
     })
